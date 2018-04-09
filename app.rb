48feaa71c85d18e686e2267aea221ccc8333cf40
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-09 21:34:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/create_puppy' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+
+    erb :create_puppy
+  end
 end
